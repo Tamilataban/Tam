41294e3d218ebdb4aca46d880f6c1f45ae6f9a61
Tam <osoaosoa99@gmail.com>
2025-01-30 20:00:09 -0600

    new file

---
+++ Test.py
@@ -0,0 +1,23 @@
+# Integers
+integer_1 = 10
+integer_2 = 0
+sum = integer_1 + integer_2
+print(sum)
+# Floating point number (Float)
+float_1 = 12.3
+float_2 = -5.2
+suma = float_1 + float_2
+print(suma)
+
+# String
+string_1 = "this is a sentence."
+string_2 = "and this is another."
+texto = string_1 + string_2
+print(texto)
+frase = string_1, string_2
+print(frase)
+# Boolean
+boolean_1 = True
+boolean_2 = False
+and = boolean_1 + boolean_2
+print(boolean_1 + boolean_2)
